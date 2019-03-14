1963fa800f4554d729f14f7f3a01a5b4af4dfbc1
A. Wilcox <AWilcox@Wilcox-Tech.com>
2019-03-14 11:14:38 -0500

    renameat: further bring towards standard behaviour

--- src/unistd/renameat.c
+++ src/unistd/renameat.c
@@ -1,16 +1,23 @@
 #include <errno.h>
+#include <fcntl.h>
 #include <libgen.h>
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "syscall.h"
 
 int renameat(int oldfd, const char *old, int newfd, const char *new)
 {
 	char old_copy[PATH_MAX+1], new_copy[PATH_MAX+1];
 	char *base;
+	size_t old_size, new_size;
+	struct stat statbuf;
 
-	if (strlen(old) > PATH_MAX || strlen(new) > PATH_MAX) {
+	if ((old_size = strlen(old)) > PATH_MAX || \
+	    (new_size = strlen(new)) > PATH_MAX) {
 		errno = ENAMETOOLONG;
 		return -1;
 	}
@@ -32,6 +39,32 @@ int renameat(int oldfd, const char *old, int newfd, const char *new)
 	    strcmp(new_copy, ".") == 0 || strcmp(new_copy, "..") == 0) {
 		errno = EINVAL;
 		return -1;
+	}
+
+	/* The Linux kernel will fail when attempting to rename a symlink of a
+	   directory with a trailing slash.  We therefore have to handle this
+	   case ourselves. */
+	if (old[old_size - 1] == '/') {
+		/* Calling stat(2) on a symlink to a dir with the trailing
+		   slash causes stat(2) to return the actual directory instead
+		   of the symlink itself. */
+		strcpy(old_copy, old);
+		old_copy[old_size - 1] = '\0';
+		if (fstatat(oldfd, old_copy, &statbuf, AT_SYMLINK_NOFOLLOW) == -1) {
+			return -1;
+		}
+		if (S_ISLNK(statbuf.st_mode)) {
+			if (fstatat(oldfd, old, &statbuf, 0) == -1) {
+				return -1;
+			}
+			if (S_ISDIR(statbuf.st_mode)) {
+				old = old_copy;
+			} else {
+				/* may as well not waste the syscall */
+				errno = ENOTDIR;
+				return -1;
+			}
+		}
 	}
 
 	return syscall(SYS_renameat, oldfd, old, newfd, new);
